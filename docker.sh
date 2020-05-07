521d3f00ff0c9db529a06d96a05e58e8ea624050
root <root@webserver-vm02.saf0aat4b2vu5pbmmisy5jgwbd.ix.internal.cloudapp.net>
2020-05-07 07:32:41 +0000

    Update installing docker-compose to docker.sh

--- docker.sh
+++ docker.sh
@@ -23,3 +23,9 @@ yum install -y docker-ce docker-ce-cli containerd.io
 #Start and enable docker
 systemctl start docker
 systemctl enable docker
+
+#Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
